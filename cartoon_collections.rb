e0367deb958aab6044c431e7a0ab3dbabc8a22a9
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-07 19:45:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,16 @@
 def greet_characters(array)
+array.each do |array|
+  x = 0
+  
+  if array = x
+    print "Hello"   
+    x += 1
+  end 
+# { |array| print array  + "\n"}
+
+
+end 
+
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
