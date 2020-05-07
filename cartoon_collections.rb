9c3e7b41a0870d6409d550dfe1b9794a1987bbb5
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-07 20:28:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |character, i |
+  end 
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
